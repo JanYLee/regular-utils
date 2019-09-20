05f79e866cfc46ffb90f6bf77d272a68877e793b
DDLancer5 <694730359@qq.com>
2019-09-21 01:41:04 +0800

    update: 更新eslint配置

--- src/metaCharacter.js
+++ src/metaCharacter.js
@@ -1,6 +1,6 @@
 export default class metaCharacter {
   static numberTest (str) {
-    console.log('"2019年"是否包含数字:', /\d/.test('2019年'))
-    return /\d/.test(str)
+    console.log('"2019年"是否包含数字:', /\d/.test('2019年'));
+    return /\d/.test(str);
   }
 }
--- test/metaCharacter.test.js
+++ test/metaCharacter.test.js
@@ -1,5 +1,5 @@
-import metaCharacter from '../src/metaCharacter.js'
+import metaCharacter from '../src/metaCharacter.js';
 
 test('\'2019年\'是否包含数字', () => {
-  expect(metaCharacter.numberTest('2019年')).toBe(true)
+  expect(metaCharacter.numberTest('2019年')).toBeTruthy();
 })
